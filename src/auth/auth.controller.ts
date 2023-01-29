35969e5b7e693de58e584fbd88f9119fedf8d9e7
TeddyKwak <joke79@hanyang.ac.kr>
2023-01-29 13:01:48 +0900

    passport intergration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { CreateUserDto } from "../user/dto/create-user.dto";
 import { LoginUserDto } from "../user/dto/login-user.dto";
+import { RequestWithUserInterface } from "./requestWithUser.interface";
+import { LocalAuthGuard } from "./guard/localAuth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -15,10 +17,15 @@ export class AuthController {
     return user;
   }
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Body() loginUserDto: LoginUserDto ){
-    const loggedUser = await this.authService.getAuthicatedUser(loginUserDto.email, loginUserDto.password)
-    return loggedUser
+  // async login(@Body() loginUserDto: LoginUserDto ){
+  //   // const loggedUser = await this.authService.getAuthicatedUser(loginUserDto.email, loginUserDto.password)
+  //   //return loggedUser;
+  // }
+  async login(@Req() request: RequestWithUserInterface){
+    const user = request.user //로그인한 상대는 유저
+    return user;
   }
 
 }
