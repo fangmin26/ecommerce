1f48fad3c30ca12d5ec8d47a5b24459e9ea97861
fangmin26 <fangmin_@naver.com>
2023-02-04 22:25:52 +0900

    httpsOptions

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { ValidationPipe } from "@nestjs/common";
 import * as fs from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('./key.pem'),
-  //   cert: fs.readFileSync('./cert.pem') //에러 생김
-  // }
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem') //에러 생김 : 파일 위치 문제였음 ^^;
+  }
 
-  const app = await NestFactory.create(AppModule, 
-    // {httpsOptions}
-    );
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useGlobalPipes(new ValidationPipe({skipMissingProperties: true}))
   await app.listen(3000);
 }
